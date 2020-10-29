72c84b9c6a40990788db952c9ac2ea440e958740
Jeremy Rodriguez <jeremy.rodrguez@yahoo.com>
2020-10-29 03:30:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,9 @@
 def greet_characters(array)
-  ["Dopey", "Grumpy", "Bashful"].each do |e|
-    puts "${e} is one of three dwarves."
+  # Use `each` to enumerate over the provided array
+  #
+  # Print a custom greeting for each element
 end
-greet_characters
+
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
